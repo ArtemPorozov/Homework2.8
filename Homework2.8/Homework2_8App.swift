af5d02e30755719369286b13412cfce8bc541859
aPorozov <a.porozov@mafin.ru>
2022-01-18 23:31:22 +0300

    Initial Commit

---
+++ Homework2.8/Homework2_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2_8App.swift
+//  Homework2.8
+//
+//  Created by Артем Порозов on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Homework2_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
